25c7fdfe19e6ac0c5da201784272e39d53628a02
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-10-27 14:30:46 +0100

    robo data

--- src/Card.js
+++ src/Card.js
@@ -1,27 +1,42 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Card = ({id}) => {
-    const [name, setName] = useState('')
+    const [users, setUsers] = useState([])
 
     const API_URL = "https://jsonplaceholder.typicode.com/users"
-    const fetchApi = async()=>{
-        const response = await axios.get(API_URL)
-        setName(response)
-    }
-    fetchApi()
+    const IMAGE_URL = `https://robohash.org/${id}100x200`
 
-    return ( 
-        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <div>
-                <img alt='' src={`https://robohash.org/${id}100x200`}/>
-            </div>
-            <div>
-                {/* <h2>{name}</h2>
-                <p>{email}</p> */}
-            </div>
+    const fetchApi = ()=>{
+        fetch(API_URL)
+        .then(response => {
+          return response.json()
+        })
+        .then(data => {
+          setUsers(data)
+        })
+        
+    }
+    useEffect (()=>{
+        fetchApi();
+    },[])
+
+    return (
+        <>
+        {users.map(user => (
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">     
+        <div>
+            <div key={user.id}>
+                <img alt='' src={IMAGE_URL}/>
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+                
+                </div>
+        </div>
 
         </div>
+          ))}
+        </> 
      );
 }
  
