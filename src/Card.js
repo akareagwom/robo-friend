948b0e36f52f1b6f6bb2727f02f83fc88ebaff5c
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-10-27 13:10:43 +0100

    user api

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-const Card = ({name, email,id}) => {
+const Card = ({id}) => {
+    const [name, setName] = useState('')
+
+    const API_URL = "https://jsonplaceholder.typicode.com/users"
+    const fetchApi = async()=>{
+        const response = await axios.get(API_URL)
+        setName(response)
+    }
+    fetchApi()
+
     return ( 
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <div>
-                <img alt='' src={`https://robohash.org/${id}`}/>
+                <img alt='' src={`https://robohash.org/${id}100x200`}/>
             </div>
             <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
+                {/* <h2>{name}</h2>
+                <p>{email}</p> */}
             </div>
 
         </div>
